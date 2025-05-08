ffe3b71ad2ca6c86d95d8c93363b59eea819cf30
Mateus Marques <mateusmarques2001@usp.br>
2025-05-08 16:01:51 -0300

    Divide and conquer Merge K Sorted Lists O(N log(k)) beats 100%

--- leetcode/23-merge_k_sorted_lists.c
+++ leetcode/23-merge_k_sorted_lists.c
@@ -5,94 +5,57 @@ struct ListNode {
 
 /**************************/
 
-#include <stdlib.h>
+#include <stdlib.h> /* NULL */
 
-#define MAX (10001)
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2);
 
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
-    struct ListNode *root = NULL;
-    int min = MAX;
-    int index = -1;
-
-    for (int i = 0; i < listsSize; i++) {
-        if (lists[i] && lists[i]->val < min) {
-            min = lists[i]->val;
-            index = i;
-        }
-    }
-
-    if (min == MAX) {
+    if (listsSize == 0) {
         return NULL;
     }
-    else {
-        root = lists[index];
-        lists[index] = lists[index]->next;
-    }
-
-    struct ListNode *node = root;
-
-    while (min != MAX) {
-        min = MAX;
-
-        for (int i = 0; i < listsSize; i++) {
-            if (lists[i] && lists[i]->val < min) {
-                min = lists[i]->val;
-                index = i;
-            }
+    while (listsSize > 1) {
+        int newListsSize = 0;
+        for (int i = 0; i + 1 < listsSize; i += 2) {
+            lists[newListsSize++] = mergeTwoLists(lists[i], lists[i+1]);
         }
-
-        if (min != MAX) {
-            node->next = lists[index];
-            node = node->next;
-            lists[index] = lists[index]->next;
+        if (listsSize % 2 == 1) {
+            lists[newListsSize++] = lists[listsSize - 1];
         }
+        listsSize = newListsSize;
     }
-
-    return root;
+    return lists[0];
 }
 
-/**************************/
-
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    if (list1 && !list2) {
-        return list1;
-    }
-    else if (!list1 && list2) {
+    if (list1 == NULL) {
         return list2;
     }
-    else if (!list1 && !list2) {
-        return NULL;
+    if (list2 == NULL) {
+        return list1;
     }
-    else {
-        struct ListNode* root;
+    struct ListNode* root;
+    if (list1->val < list2->val) {
+        root = list1;
+        list1 = list1->next;
+    } else {
+        root = list2;
+        list2 = list2->next;
+    }
+    struct ListNode* node = root;
+    while (list1 && list2) {
         if (list1->val < list2->val) {
-            root = list1;
+            node->next = list1;
             list1 = list1->next;
-        }
-        else {
-            root = list2;
+        } else {
+            node->next = list2;
             list2 = list2->next;
         }
-        struct ListNode* node = root;
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                node->next = list1;
-                node = node->next;
-                list1 = list1->next;
-            }
-            else {
-                node->next = list2;
-                node = node->next;
-                list2 = list2->next;
-            }
-        }
-        if (list1) {
-            node->next = list1;
-            return root;
-        }
-        else {
-            node->next = list2;
-            return root;
-        }
+        node = node->next;
     }
+    if (list1) {
+        node->next = list1;
+    } else {
+        node->next = list2;
+    }
+    return root;
 }
