f3ca86da344f3c61610e2c0611703d7b994568b2
Mateus Marques <mateusmarques2001@usp.br>
2025-05-14 18:34:08 -0300

    Some comment explaining 796-rotate_string.c

--- leetcode/796-rotate_string.c
+++ leetcode/796-rotate_string.c
@@ -5,12 +5,17 @@
 
 #include <string.h> /* strlen */
 
+/* in order for goal to be shift of s, two things are necessary: */
+/*      1. they have the same length */
+/*      2. goal is a substring of s+s (concatenation of s with itself) */
 bool rotateString(char* s, char* goal) {
+    /* check if they have the same length */
     int len_s = strlen(s), len_goal = strlen(goal);
     if (len_s != len_goal) {
         return false;
     }
 
+    /* check if goal is a substring of s+s */
     int i = 0, num_turns = 0;
     int j = 0;
     while (num_turns != 2) {
