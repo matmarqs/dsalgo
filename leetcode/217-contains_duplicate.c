0b325bf337f2ec8328fb3f555e38eb60b7ea78ca
Mateus Marques <203121612+gn-mm@users.noreply.github.com>
2025-05-27 09:12:18 -0300

    Simple HashSet O(n) implementation that beats 95%

--- leetcode/217-contains_duplicate.c
+++ leetcode/217-contains_duplicate.c
@@ -4,16 +4,78 @@
 
 #include <stdlib.h>
 
-int cmp_int(const void *a, const void *b) {
-    return *((int *)a) - *((int *) b);
+typedef long long ll;
+
+typedef struct {
+    ll *keys;
+    bool *occupied;
+    int size;
+} HashSet;
+
+HashSet *set_create(int size) {
+    HashSet *set = (HashSet *) malloc(sizeof(HashSet));
+    set->keys = (ll *) calloc(size, sizeof(ll));
+    set->occupied = (bool *) calloc(size, sizeof(bool));
+    set->size = size;
+    return set;
 }
 
+void set_free(HashSet *set) {
+    free(set->keys);
+    free(set->occupied);
+    free(set);
+}
 
-bool containsDuplicateSorting(int* nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), cmp_int);
-    for (int i = 1; i < numsSize; i++) {
-        if (nums[i] == nums[i-1])
+/* simple hash function with prime number */
+int hash(ll key, int size) {
+    return (int) ((key * 31LL) % size + size) % size;
+    // a % b is not a modulo operator, but a remainder operator
+    // a % b can return negative values
+}
+
+bool contains(ll key, HashSet *set) {
+    int index = hash(key, set->size);
+    int original_index = index;
+
+    while (set->occupied[index]) {
+        if (set->keys[index] == key) {
             return true;
+        }
+        index = (index + 1) % set->size;
+        if (index == original_index) // this prevents an infinite loop
+            return false;
+        // in the worst case scenario, we take O(N) to check
+        // but, on average we take O(1)
     }
+    return false;
+}
+
+void add(ll key, HashSet *set) {
+    int index = hash(key, set->size);
+
+    while (set->occupied[index]) {
+        if (set->keys[index] == key) {
+            return; // it's already in the set
+        }
+        index = (index + 1) % set->size; // possible infinite loop, if the set is full
+    }
+
+    set->keys[index] = key;
+    set->occupied[index] = true;
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+
+    HashSet *set = set_create(2 * numsSize); // 2 * numsSize in order to minimize collisions
+
+    for (int i = 0; i < numsSize; i++) {
+        if (contains(nums[i], set)) {
+            set_free(set);
+            return true;
+        }
+        add(nums[i], set);
+    }
+
+    set_free(set);
     return false;
 }
