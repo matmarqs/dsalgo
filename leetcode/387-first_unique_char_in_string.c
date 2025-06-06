84eee92f15e1a412f9c09e6c0114086e2f449e81
Mateus Marques <203121612+gn-mm@users.noreply.github.com>
2025-06-06 10:29:01 -0300

    Problem 387: First Uniq Char, with Queue

--- leetcode/387-first_unique_char_in_string.c
+++ leetcode/387-first_unique_char_in_string.c
@@ -1,31 +1,102 @@
-#define INFINITY 1000000
+#include <stdlib.h>
+
+typedef enum { in, not_in, ignore } state;
+
+typedef struct Node {
+    char c;
+    int index;
+    struct Node *next;
+} Node;
+
+Node *create_node(char c, int index) {
+    Node *node = (Node *) malloc(sizeof(Node));
+    node->c = c;
+    node->index = index;
+    node->next = NULL;
+    return node;
+}
 
 typedef struct {
-    int first_index;
-    int count;
-} Repeat;
+    Node *head;
+    Node *tail;
+    int size;
+} Queue;
+
+Queue *init_queue() {
+    Queue *q = (Queue *) malloc(sizeof(Queue));
+    q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
+    return q;
+}
+
+int peek(Queue *q) {
+    return q->head->c;
+}
+
+int peek_index(Queue *q) {
+    return q->head->index;
+}
+
+int is_empty(Queue *q) {
+    return q->size == 0;
+}
+
+void dequeue(Queue *q) {
+    if (is_empty(q)) {
+        exit(1);    /* ERROR: queue is empty, cannot dequeue */
+    }
+    Node *old_head = q->head;
+    q->head = q->head->next;
+    free(old_head);
+    q->size--;
+}
+
+void enqueue(Queue *q, char c, int index) {
+    Node *new_tail = create_node(c, index);
+    if (is_empty(q)) {
+        q->head = new_tail;
+        q->tail = new_tail;
+    }
+    else {
+        q->tail->next = new_tail;
+        q->tail = new_tail;
+    }
+    q->size++;
+}
+
+void free_queue(Queue *q) {
+    while (q->head) {
+        Node *current = q->head;
+        q->head = q->head->next;
+        free(current);
+    }
+    free(q);
+}
 
 int firstUniqChar(char* s) {
-    Repeat repeat[26];
-
-    for (int c = 0; c < 26; c++) {
-        repeat[c].first_index = INFINITY;
-        repeat[c].count = 0;
+    Queue *q = init_queue();
+    state in_queue[26];
+    for (int i = 0; i < 26; i++) {
+        in_queue[i] = not_in;
     }
 
     for (int i = 0; s[i] != '\0'; i++) {
-        if (repeat[s[i] - 'a'].first_index == INFINITY) {
-            repeat[s[i] - 'a'].first_index = i;
+        char ch = s[i];
+
+        if (in_queue[ch - 'a'] == not_in) {
+            enqueue(q, ch, i);
+            in_queue[ch - 'a'] = in;
         }
-        repeat[s[i] - 'a'].count++;
+        else if (in_queue[ch - 'a'] == in) {
+            in_queue[ch - 'a'] = ignore;
+        }
+        while (!is_empty(q) && in_queue[peek(q) - 'a'] == ignore) {
+            dequeue(q);
+        }
     }
 
-    int first_uniq = INFINITY;
-    for (int c = 0; c < 26; c++) {
-        if (repeat[c].count == 1 && repeat[c].first_index < first_uniq) {
-            first_uniq = repeat[c].first_index;
-        }
-    }
-
-    return first_uniq == INFINITY ? -1 : first_uniq;
+    int result = is_empty(q) ? -1 : peek_index(q);
+    free_queue(q);
+    return result;
 }
