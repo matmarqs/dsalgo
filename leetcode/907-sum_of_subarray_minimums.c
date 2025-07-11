bdb636d2a4a1a11c7d03e4feacfebe82cad82503
Mateus Marques <203121612+gn-mm@users.noreply.github.com>
2025-07-11 11:52:36 -0300

    Problem 907: Letting the code a little more elegant

--- leetcode/907-sum_of_subarray_minimums.c
+++ leetcode/907-sum_of_subarray_minimums.c
@@ -10,7 +10,6 @@
 /* left is the number of positions that a subarray can start */
 /* right is the number of positions that a subarray can end */
 
-
 /* O(n) time, O(n) space */
 int sumSubarrayMins(int* arr, int arrSize) {
     const int MOD = 1e9 + 7;
@@ -21,21 +20,14 @@ int sumSubarrayMins(int* arr, int arrSize) {
     int top = -1;
 
     long long left, right;
-    for (int i = 0; i < arrSize; i++) {
-        while (top >= 0 && arr[i] <= arr[stack[top]]) {
+    for (int i = 0; i <= arrSize; i++) {
+        while (top >= 0 && (i == arrSize || arr[i] <= arr[stack[top]])) {
             left = stack[top] - (top > 0 ? stack[top-1] : -1);
             right = i - stack[top];
             sum = (sum + ((long long) arr[stack[top]]) * left * right) % MOD;
             top--;
         }
         stack[++top] = i;
-    }
-
-    while (top >= 0) {
-        left = stack[top] - (top > 0 ? stack[top-1] : -1);
-        right = arrSize - stack[top];
-        sum = (sum + ((long long) arr[stack[top]]) * left * right) % MOD;
-        top--;
     }
 
     free(stack);
