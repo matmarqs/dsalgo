de0f9b6d0481e8d4d866f9e2c7026ccd39d373ba
Mateus Marques <203121612+gn-mm@users.noreply.github.com>
2025-07-11 11:31:25 -0300

    Problem 907: Correct solution now. The goal of the monotonic stack is to find prev_min and next_min, then calculate sum accordingly

--- leetcode/907-sum_of_subarray_minimums.c
+++ leetcode/907-sum_of_subarray_minimums.c
@@ -4,56 +4,45 @@
 
 #include <stdlib.h>
 
+/* we use a Monotonic Stack to find prev_smaller and next_smaller */
+/* increase the sum by arr[stack[top]] * left * right */
+/* considering the subarrays that arr[stack[top]] is a minimum: */
+/* left is the number of positions that a subarray can start */
+/* right is the number of positions that a subarray can end */
+
+
+/* O(n) time, O(n) space */
 int sumSubarrayMins(int* arr, int arrSize) {
-    int sum = 0;
+    const int MOD = 1e9 + 7;
+    long long sum = 0;
 
-    /* monotonic increasing stack */
-    int *stack = (int *) malloc(sizeof(int) * arrSize);
+    // Increasing monotonic stack to find previous and next smaller elements
+    int *stack = (int *)malloc(arrSize * sizeof(int));
     int top = -1;
-    stack[++top] = 0;
 
-    int i = 1;
-    while (i < arrSize) {
-        if (top >= 0 && arr[i] <= arr[stack[top]]) {
-            sum += arr[i] * (i - stack[top] - 1);
-        }
+    long long left, right;
+    for (int i = 0; i < arrSize; i++) {
         while (top >= 0 && arr[i] <= arr[stack[top]]) {
-            sum += arr[stack[top]] * (i - stack[top]);
-            //printf("Retirando do stack\n");
-            //printf("top = %d\n", arr[stack[top]]);
+            left = stack[top] - (top > 0 ? stack[top-1] : -1);
+            right = i - stack[top];
+            sum = (sum + ((long long) arr[stack[top]]) * left * right) % MOD;
             top--;
         }
         stack[++top] = i;
-        i++;
     }
 
-    /* now we empty the stack until it has only one element */
-    int stack_size = top+1;
-    //printf("stack_size = %d\n", top+1);
-    while (top >= 1) {
-        sum += arr[stack[top]] * (arrSize - stack[top]);
-        //printf("Retirando do stack\n");
-        //printf("top = %d\n", arr[stack[top]]);
+    while (top >= 0) {
+        left = stack[top] - (top > 0 ? stack[top-1] : -1);
+        right = arrSize - stack[top];
+        sum = (sum + ((long long) arr[stack[top]]) * left * right) % MOD;
         top--;
     }
-    /* the bottom element of the stack is the global minimum */
-    sum += arr[stack[top]] * (arrSize + stack_size - 1); /* it is the minimum n arrays not counted yet */
-    /* [0, 1, ..., min-1, min] */
-    /* [1, ..., min-1, min] */
-    /* ... */
-    /* [min-1, min] */
-    /* [min] */
-    /* [min, min+1] */
-    /* ... */
-    /* [min, min+1, ..., n-1] */
-    top--;
-    /* now the stack is empty */
 
     free(stack);
-    return sum;
+    return (int) sum;
 }
 
-/************************************/
+/******************************************/
 
 int main() {
     int arr[] = { 11,81,94,43,3 };
